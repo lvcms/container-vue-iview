559cbee703b20f383ed0390e9b70ef60105684c9
bigrocs <bigrocs@qq.com>
2018-04-27 22:22:47 +0800

    响应式

--- src/components/Layout/Content.vue
+++ src/components/Layout/Content.vue
@@ -9,4 +9,16 @@
   }
 </script>
 <style lang="scss" scoped>
+@import  './../../assets/sass/mixin.scss';
+.ivu-layout-content{
+  margin-top: 1vh;
+  margin-bottom: 1vh;
+}
+@media (min-width: $md) {
+  /* 屏幕小的时候自动隐藏 breadcrumb */
+  .ivu-layout-content{
+    margin-left: 1vh;
+    margin-right: 1vh;
+  }
+}
 </style>
