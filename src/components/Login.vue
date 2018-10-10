69a89b44a2627df7ffae5a0bfffc44417a515689
bigrocs <bigrocs@qq.com>
2018-10-10 10:50:33 +0800

    修改背景图片

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="login-body" id="loginBody">
+<div class="login-body" :class="loginClass">
   <div class="login-item">
     <router-view></router-view>
   </div>
@@ -9,6 +9,12 @@
   import Cache from 'lf-cache'
   export default {
     name: 'cvi-login',
+    data () {
+      return {
+          // 随便背景颜色
+          loginClass: 'login-body-'+Math.ceil(Math.random()*27)
+      }
+    },
     created() {
       this.eventOn()
     },
@@ -57,16 +63,8 @@
        */
       backgroundImage() {
           let backgroundImage = () =>{
-              let loginBody = document.getElementById("loginBody")
-              if (loginBody) {
-                let num = Math.ceil(Math.random()*27)
-                loginBody.style.backgroundImage="url(/vendor/admin/background/"+ num +".jpg)"
-              }else{
-                // 防止不断检测 找不到 loginBody
-                clearInterval(outTime)
-              }
+            this.loginClass = 'login-body-' + Math.ceil(Math.random()*27)
           }
-          backgroundImage()
           let outTime = setInterval(backgroundImage,10000)
       }
     }
@@ -88,4 +86,87 @@
 .login-item{
   margin-bottom: 6.18%;
 }
+
+.login-body-1{
+  background-image:url('../assets/img/1.jpg');
+}
+.login-body-2{
+  background-image:url('../assets/img/2.jpg');
+}
+.login-body-3{
+  background-image:url('../assets/img/3.jpg');
+}
+.login-body-4{
+  background-image:url('../assets/img/4.jpg');
+}
+.login-body-5{
+  background-image:url('../assets/img/5.jpg');
+}
+.login-body-6{
+  background-image:url('../assets/img/6.jpg');
+}
+.login-body-7{
+  background-image:url('../assets/img/7.jpg');
+}
+.login-body-8{
+  background-image:url('../assets/img/8.jpg');
+}
+.login-body-9{
+  background-image:url('../assets/img/9.jpg');
+}
+.login-body-10{
+  background-image:url('../assets/img/10.jpg');
+}
+.login-body-11{
+  background-image:url('../assets/img/11.jpg');
+}
+.login-body-12{
+  background-image:url('../assets/img/12.jpg');
+}
+.login-body-13{
+  background-image:url('../assets/img/13.jpg');
+}
+.login-body-14{
+  background-image:url('../assets/img/14.jpg');
+}
+.login-body-15{
+  background-image:url('../assets/img/15.jpg');
+}
+.login-body-16{
+  background-image:url('../assets/img/16.jpg');
+}
+.login-body-17{
+  background-image:url('../assets/img/17.jpg');
+}
+.login-body-18{
+  background-image:url('../assets/img/18.jpg');
+}
+.login-body-19{
+  background-image:url('../assets/img/19.jpg');
+}
+.login-body-20{
+  background-image:url('../assets/img/20.jpg');
+}
+.login-body-21{
+  background-image:url('../assets/img/21.jpg');
+}
+.login-body-22{
+  background-image:url('../assets/img/22.jpg');
+}
+.login-body-23{
+  background-image:url('../assets/img/23.jpg');
+}
+.login-body-24{
+  background-image:url('../assets/img/24.jpg');
+}
+.login-body-25{
+  background-image:url('../assets/img/25.jpg');
+}
+.login-body-26{
+  background-image:url('../assets/img/26.jpg');
+}
+.login-body-27{
+  background-image:url('../assets/img/27.jpg');
+}
+
 </style>
